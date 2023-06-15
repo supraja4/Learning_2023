8c1747e6d258f226d61b286e6a995b2ea32864c0
Supraja <suprajasuppu175@gmail.com>
2023-06-15 12:00:24 +0530

    commit 1 : main : Day1 Activities added

---
+++ Max.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter two numbers:",a,b);
+    scanf("%d %d",&a,&b);
+
+    if(a>b)
+    {
+       printf("%d is greater than %d",a,b);
+    }
+    else 
+    {
+       printf("%d is greater than %d",b,a);;
+    }
+
+    return 0;
+}
